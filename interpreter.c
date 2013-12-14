21ab734e21ce589f5ffa16c5f3a78ca9d8d765e9
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-12-14 14:30:06 -0800

    Implemented more of the interpreter

--- interpreter.c
+++ interpreter.c
@@ -2,66 +2,168 @@
 #include <libc.h>
 #include "interpreter.h"
 void main() {
-   processor proc;
-	
+	processor proc;
+	setupprocessor(&proc);
 	exits(0);
 }
+void setupprocessor(processor* proc) {
+	/* Upper registers have a special purpose */
+	/* false/zero */
+	proc->gpr[FalseRegister] = 0;
+	/* true/one */
+	proc->gpr[TrueRegister] = 1;
+	/* processor-id */
+	proc->gpr[ProcessorIdRegister] = processor_count++;
+	/* register count */
+	proc->gpr[RegisterCountRegister] = 128;
+	/* cell count [64-bits * total count] */
+	proc->gpr[CellCountRegister] = 131072;
+	/* program-counter */
+	proc->gpr[ProgramCounter] = 2048;
 
-void add(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1) {
-	if(proc->gpr[predicate]) {
-		proc->gpr[dest] = proc->gpr[src0] + proc->gpr[src1];	
-	}
+}
+instruction retrieveinstruction(processor* proc) {
+	return (instruction)proc->memory[proc->programcounter];
+}
+void incrementprogramcounter(processor* proc) {
+	proc->gpr[ProgramCounter]++;
+}
+int instructionexecutable(processor* proc, instruction inst) {
+	return proc->gpr[inst.bytes[0]];
+}
+void cycle(processor* proc) {
+	instruction a, b;
+	a = retrieveinstruction(proc);
+	/* check to see if we should execute */
+	if(instructionexecutable(proc, a)) {
+		switch(inst.bytes[1]) {
+			case 0:
+				nop(proc);
+				break;
+			case 1:
+				add(proc, a.bytes[2], a.bytes[4], a.bytes[5]);
+				break;
+			case 2:
+				sub(proc, a.bytes[2], a.bytes[4], a.bytes[5]);
+				break;
+			case 3:
+				mul(proc, a.bytes[2], a.bytes[4], a.bytes[5]);
+				break;
+			case 4:
+				divop(proc, a.bytes[2], a.bytes[4], a.bytes[5]);
+				break;
+			case 5:
+				rightshift(proc, a.bytes[2], a.bytes[4], a.bytes[5]);
+				break;
+			case 6:
+				leftshift(proc, a.bytes[2], a.bytes[4], a.bytes[5]);
+				break;
+			case 7:
+				binaryor(proc, a.bytes[2], a.bytes[4], a.bytes[5]);
+				break;
+			case 8:
+				binaryand(proc, a.bytes[2], a.bytes[4], a.bytes[5]);
+				break;
+			case 9:
+				binarynot(proc, a.bytes[2], a.bytes[4]);
+				break;
+			case 10:
+				equals(proc, a.bytes[2], a.bytes[3], a.bytes[4], a.bytes[5]);
+				break;
+			case 11:
+				notequals(proc, a.bytes[2], a.bytes[3], a.bytes[4], a.bytes[5]);
+				break;
+			case 12:
+				greaterthan(proc, a.bytes[2], a.bytes[3], a.bytes[4], a.bytes[5]);
+				break;
+			case 13:
+				lessthan(proc, a.bytes[2], a.bytes[3], a.bytes[4], a.bytes[5]);
+				break;
+			case 14:
+				load(proc, a.bytes[2], a.bytes[4]);
+				break;
+			case 15:
+				store(proc, a.bytes[2], a.bytes[4]);
+				break;
+			case 16:
+				/* we need to grab the next cell */
+				incrementprogramcounter(proc);
+				b = retrieveinstruction(proc);
+				branch(proc, b.value);
+				break;
+			case 17:
+				/* this is an interesting case */
+				incrementprogramcounter(proc);
+				b = retrieveinstruction(proc);
+				set(proc, a.bytes[2], b.value);
+		}
+	} 
+	incrementprogramcounter(proc);
+}
+void add(processor* proc, uchar dest, uchar src0, uchar src1) {
+	proc->gpr[dest] = proc->gpr[src0] + proc->gpr[src1];	
 }
 
-void sub(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1) {
-	if(proc->gpr[predicate]) {
-		proc->gpr[dest] = proc->gpr[src0] - proc->gpr[src1];	
-	}
+void sub(processor* proc, uchar dest, uchar src0, uchar src1) {
+	proc->gpr[dest] = proc->gpr[src0] - proc->gpr[src1];	
+}
+void mul(processor* proc, uchar dest, uchar src0, uchar src1) {
+	proc->gpr[dest] = proc->gpr[src0] * proc->gpr[src1];	
+}
+void divop(processor* proc, uchar dest, uchar src0, uchar src1) {
+	proc->gpr[dest] = proc->gpr[src0] / proc->gpr[src1];	
+}
+void rightshift(processor* proc, uchar dest, uchar src0, uchar src1) {
+	proc->gpr[dest] = proc->gpr[src0] >> proc->gpr[src1];	
+}
+void leftshift(processor* proc, uchar dest, uchar src0, uchar src1) {
+	proc->gpr[dest] = proc->gpr[src0] << proc->gpr[src1];	
+}
+void binaryor(processor* proc, uchar dest, uchar src0, uchar src1) {
+	proc->gpr[dest] = proc->gpr[src0] | proc->gpr[src1];	
+}
+void binaryand(processor* proc, uchar dest, uchar src0, uchar src1) {
+	proc->gpr[dest] = proc->gpr[src0] & proc->gpr[src1];	
+}
+void binaryxor(processor* proc, uchar dest, uchar src0, uchar src1) {
+	proc->gpr[dest] = proc->gpr[src0] ^ proc->gpr[src1];	
+}
+void binarynot(processor* proc, uchar dest, uchar src0) {
+	proc->gpr[dest] = !(proc->gpr[src0]);	
+}
+void equals(processor* proc, uchar desttrue, uchar destfalse, uchar src0, uchar src1) {
+	uvlong tmp;
+	tmp = proc->gpr[src0] == proc->gpr[src1];	
+	proc->gpr[desttrue] = tmp;
+	proc->gpr[destfalse] = !tmp;
+}
+void notequals(processor* proc, uchar desttrue, uchar destfalse, uchar src0, uchar src1) {
+	equals(proc, destfalse, desttrue, src0, src1);
+}
+void greaterthan(processor* proc, uchar desttrue, uchar destfalse, uchar src0, uchar src1) {
+	uvlong tmp;
+	tmp = proc->gpr[src0] > proc->gpr[src1];
+	proc->gpr[desttrue] = tmp;
+	proc->gpr[destfalse] = !tmp;
+}
+void lessthan(processor* proc, uchar desttrue, uchar destfalse, uchar src0, uchar src1) {
+	uvlong tmp;
+	tmp = proc->gpr[src0] < proc->gpr[src1];
+	proc->gpr[desttrue] = tmp;
+	proc->gpr[destfalse] = !tmp;
+}
 
+void load(processor* proc, uchar dest, uchar src) {
+	proc->gpr[dest] = proc->memory[proc->gpr[src]];
 }
-void mul(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1) {
-	if(proc->gpr[predicate]) {
-		proc->gpr[dest] = proc->gpr[src0] * proc->gpr[src1];	
-	}
+void store(processor* proc, uchar dest, uchar src) {
+	proc->memory[proc->gpr[dest]] = proc->gpr[src];
 }
-void div(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1) {
-	if(proc->gpr[predicate]) {
-		proc->gpr[dest] = proc->gpr[src0] / proc->gpr[src1];	
-	}
-}
-void rightshift(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1) {
-	if(proc->gpr[predicate]) {
-		proc->gpr[dest] = proc->gpr[src0] >> proc->gpr[src1];	
-	}
-}
-void leftshift(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1) {
-	if(proc->gpr[predicate]) {
-		proc->gpr[dest] = proc->gpr[src0] << proc->gpr[src1];	
-	}
-}
-void binaryor(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1) {
-	if(proc->gpr[predicate]) {
-		proc->gpr[dest] = proc->gpr[src0] | proc->gpr[src1];	
-	}
-}
-void binaryand(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1) {
-	if(proc->gpr[predicate]) {
-		proc->gpr[dest] = proc->gpr[src0] & proc->gpr[src1];	
-	}
-}
-void binaryxor(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1) {
-	if(proc->gpr[predicate]) {
-		proc->gpr[dest] = proc->gpr[src0] ^ proc->gpr[src1];	
-	}
-	
-}
-void binarynot(processor* proc, uchar predicate, uchar dest, uchar src0);
-void equals(processor* proc, uchar predicate, uchar desttrue, uchar destfalse, uchar src0, uchar src1);
-void notequals(processor* proc, uchar predicate, uchar desttrue, uchar destfalse, uchar src0, uchar src1);
-void greaterthan(processor* proc, uchar predicate, uchar desttrue, uchar destfalse, uchar src0, uchar src1);
-void lessthan(processor* proc, uchar predicate, uchar desttrue, uchar destfalse, uchar src0, uchar src1);
 
-void load(processor* proc, uchar predicate, uchar dest, uchar src);
-void store(processor* proc, uchar predicate, uchar dest, uchar src);
+void branch(processor* proc, uvlong dest) {
+	proc->programcounter = dest;
+}
 
-void branch(processor* proc, uchar predicate, uvlong dest);
+void set(processor* proc, uchar dest, uvlong value) {
+		proc->gpr[dest] = value;
+}
--- interpreter.h
+++ interpreter.h
@@ -1,30 +1,78 @@
+#define FalseRegister 127
+#define TrueRegister 126
+#define ProcessorIdRegister 125
+#define RegisterCountRegister 124
+#define CellCountRegister 123
+#define ProgramCounter 122
+uvlong processor_count = 0;
 typedef struct processor {
 	uvlong gpr[128];
-	uvlong programcounter;
-	memorycell memory[32768];
+	uvlong memory[131072];
 } processor;
-
-typedef union memorycell {
+typedef union instruction {
 	uvlong value;
 	uchar bytes[8];
-} memorycell;
+	/* an instruction is made up of eight parts 
+	 * 0) predicate ind
+	 * 1) instruction id
+	 * 2) destination (true)
+	 * 3) destination (false)
+	 * 4) src0 ind
+	 * 5) src1 ind
+	 * 6) src2 ind
+	 * 7) src3 ind
+	 */
+} instruction;
 
-void add(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1);
-void sub(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1);
-void mul(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1);
-void div(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1);
-void rightshift(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1);
-void leftshift(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1);
-void binaryor(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1);
-void binaryand(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1);
-void binaryxor(processor* proc, uchar predicate, uchar dest, uchar src0, uchar src1);
-void binarynot(processor* proc, uchar predicate, uchar dest, uchar src0);
-void equals(processor* proc, uchar predicate, uchar desttrue, uchar destfalse, uchar src0, uchar src1);
-void notequals(processor* proc, uchar predicate, uchar desttrue, uchar destfalse, uchar src0, uchar src1);
-void greaterthan(processor* proc, uchar predicate, uchar desttrue, uchar destfalse, uchar src0, uchar src1);
-void lessthan(processor* proc, uchar predicate, uchar desttrue, uchar destfalse, uchar src0, uchar src1);
+/* translation table
+ * 0) nop
+ * 1) add
+ * 2) sub
+ * 3) mul
+ * 4) div
+ * 5) rightshift
+ * 6) leftshift
+ * 7) binaryor
+ * 8) binaryand
+ * 9) binarynot
+ * 10) equals
+ * 11) notequals
+ * 12) greaterthan
+ * 13) lessthan
+ * 14) load
+ * 15) store
+ * 16) branch
+ * 17) set
+ */
 
-void load(processor* proc, uchar predicate, uchar dest, uchar src);
-void store(processor* proc, uchar predicate, uchar dest, uchar src);
 
-void branch(processor* proc, uchar predicate, uvlong dest);
+void nop(processor* proc);
+void add(processor* proc, uchar dest, uchar src0, uchar src1);
+void sub(processor* proc, uchar dest, uchar src0, uchar src1);
+void mul(processor* proc, uchar dest, uchar src0, uchar src1);
+void divop(processor* proc, uchar dest, uchar src0, uchar src1);
+void rightshift(processor* proc, uchar dest, uchar src0, uchar src1);
+void leftshift(processor* proc, uchar dest, uchar src0, uchar src1);
+void binaryor(processor* proc, uchar dest, uchar src0, uchar src1);
+void binaryand(processor* proc, uchar dest, uchar src0, uchar src1);
+void binaryxor(processor* proc, uchar dest, uchar src0, uchar src1);
+void binarynot(processor* proc, uchar dest, uchar src0);
+void equals(processor* proc, uchar desttrue, uchar destfalse, uchar src0, uchar src1);
+void notequals(processor* proc, uchar desttrue, uchar destfalse, uchar src0, uchar src1);
+void greaterthan(processor* proc, uchar desttrue, uchar destfalse, uchar src0, uchar src1);
+void lessthan(processor* proc, uchar desttrue, uchar destfalse, uchar src0, uchar src1);
+
+void load(processor* proc, uchar dest, uchar src);
+void store(processor* proc, uchar dest, uchar src);
+
+void branch(processor* proc, uvlong dest);
+void set(processor* proc, uchar dest, uvlong value);
+
+/* helper routines */
+void incrementprogramcounter(processor* proc);
+instruction retrieveinstruction(processor* proc);
+void parseinstruction(processor* proc, instruction inst);
+int instructionexecutable(processor* proc, instruction inst);
+
+/* bios routines */
+void setupprocessor(processor* proc);
