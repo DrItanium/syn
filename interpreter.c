296bdcf98c1fe7f27337c822502c998def8c3c63
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-12-14 17:34:28 -0800

    Started writing bios routines

--- interpreter.c
+++ interpreter.c
@@ -7,93 +7,69 @@ void main() {
 	while(cycle(&proc));
 	exits(0);
 }
-void setupprocessor(processor* proc) {
-	instruction nop;
-	instruction branch0, branch1;
-	
-	/* Upper registers have a special purpose */
-	proc->gpr[FalseRegister] = 0;
-	proc->gpr[TrueRegister] = 1;
-	proc->gpr[ProcessorIdRegister] = processor_count++;
-	proc->gpr[RegisterCountRegister] = 128;
-	proc->gpr[CellCountRegister] = 131072;
-	/* program-counter */
-	proc->gpr[ProgramCounter] = 2048;
-	/* just loop at this point */
-	nop.value = 0;
-	nop.bytes[InstructionPredicate] = TrueRegister;
-	nop.bytes[InstructionId] = NopInstruction;
-	branch0.value = 0;
-	branch0.bytes[InstructionPredicate] = TrueRegister;
-	branch0.bytes[InstructionId] = BranchInstruction;
-	branch1.value = proc->gpr[ProgramCounter];
-	proc->memory[proc->gpr[ProgramCounter]] = nop.value;
-
-	proc->memory[proc->gpr[ProgramCounter] + 1] = branch0.value;
-	proc->memory[proc->gpr[ProgramCounter] + 2] = branch1.value;
-}
 instruction retrieveinstruction(processor* proc) {
 	return (instruction)proc->memory[proc->gpr[ProgramCounter]];
 }
 void incrementprogramcounter(processor* proc) {
-	proc->gpr[ProgramCounter]++;
+	add(proc, ProgramCounter, ProgramCounter, TrueRegister);
+	modop(proc, ProgramCounter, ProgramCounter, CellCountRegister);
 }
 int instructionexecutable(processor* proc, instruction inst) {
-	return proc->gpr[inst.bytes[InstructionPredicate]];
+	return proc->gpr[inst.predicate];
 }
 int cycle(processor* proc) {
 	instruction a, b;
 	a = retrieveinstruction(proc);
 	/* check to see if we should execute */
 	if(instructionexecutable(proc, a)) {
-		switch(a.bytes[InstructionId]) {
+		switch(a.id) {
 			case NopInstruction:
 				nop(proc);
 				break;
 			case AddInstruction:
-				add(proc, a.bytes[InstructionDestination0], a.bytes[InstructionSource0], a.bytes[InstructionSource1]);
+				add(proc, a.destination0, a.source0, a.source1);
 				break;
 			case SubInstruction:
-				sub(proc, a.bytes[InstructionDestination0], a.bytes[InstructionSource0], a.bytes[InstructionSource1]);
+				sub(proc, a.destination0, a.source0, a.source1);
 				break;
 			case MulInstruction:
-				mul(proc, a.bytes[InstructionDestination0], a.bytes[InstructionSource0], a.bytes[InstructionSource1]);
+				mul(proc, a.destination0, a.source0, a.source1);
 				break;
 			case DivInstruction:
-				divop(proc, a.bytes[InstructionDestination0], a.bytes[InstructionSource0], a.bytes[InstructionSource1]);
+				divop(proc, a.destination0, a.source0, a.source1);
 				break;
 			case RightShiftInstruction:
-				rightshift(proc, a.bytes[InstructionDestination0], a.bytes[InstructionSource0], a.bytes[InstructionSource1]);
+				rightshift(proc, a.destination0, a.source0, a.source1);
 				break;
 			case LeftShiftInstruction:
-				leftshift(proc, a.bytes[InstructionDestination0], a.bytes[InstructionSource0], a.bytes[InstructionSource1]);
+				leftshift(proc, a.destination0, a.source0, a.source1);
 				break;
 			case BinaryOrInstruction:
-				binaryor(proc, a.bytes[InstructionDestination0], a.bytes[InstructionSource0], a.bytes[InstructionSource1]);
+				binaryor(proc, a.destination0, a.source0, a.source1);
 				break;
 			case BinaryAndInstruction:
-				binaryand(proc, a.bytes[InstructionDestination0], a.bytes[InstructionSource0], a.bytes[InstructionSource1]);
+				binaryand(proc, a.destination0, a.source0, a.source1);
 				break;
 			case BinaryNotInstruction:
-				binarynot(proc, a.bytes[InstructionDestination0], a.bytes[InstructionSource0]);
+				binarynot(proc, a.destination0, a.source0);
 				break;
 			case EqualsInstruction:
-				equals(proc, a.bytes[InstructionDestination0], a.bytes[InstructionDestination1], a.bytes[InstructionSource0], a.bytes[InstructionSource1]);
+				equals(proc, a.destination0, a.destination1, a.source0, a.source1);
 				break;
 			case NotEqualsInstruction:
-				notequals(proc, a.bytes[InstructionDestination0], a.bytes[InstructionDestination1], a.bytes[InstructionSource0], a.bytes[InstructionSource1]);
+				notequals(proc, a.destination0, a.destination1, a.source0, a.source1);
 				break;
 			case GreaterThanInstruction:
-				greaterthan(proc, a.bytes[InstructionDestination0], a.bytes[InstructionDestination1], a.bytes[InstructionSource0], a.bytes[InstructionSource1]);
+				greaterthan(proc, a.destination0, a.destination1, a.source0, a.source1);
 				break;
 			case LessThanInstruction:
-				lessthan(proc, a.bytes[InstructionDestination0], a.bytes[InstructionDestination1], a.bytes[InstructionSource0], a.bytes[InstructionSource1]);
+				lessthan(proc, a.destination0, a.destination1, a.source0, a.source1);
 				break;
 			case LoadInstruction:
-				load(proc, a.bytes[InstructionDestination0], a.bytes[InstructionSource0]);
+				load(proc, a.destination0, a.source0);
 				break;
 			case StoreInstruction:
-				store(proc, a.bytes[InstructionDestination0], a.bytes[InstructionSource0]);
+				store(proc, a.destination0, a.source0);
 				break;
 			case BranchInstruction:
 				/* we need to grab the next cell */
@@ -105,10 +81,19 @@ int cycle(processor* proc) {
 				/* this is an interesting case */
 				incrementprogramcounter(proc);
 				b = retrieveinstruction(proc);
-				set(proc, a.bytes[InstructionDestination0], b.value);
+				set(proc, a.destination0, b.value);
 				break;
 			case ModInstruction:
-				modop(proc, a.bytes[InstructionDestination0], a.bytes[InstructionSource0], a.bytes[InstructionSource1]);
+				modop(proc, a.destination0, a.source0, a.source1);
+				break;
+			case CallInstruction:
+				call(proc, b.source0);
+				break;
+			case RetInstruction:
+				ret(proc);
+				break;
+			case PlatformCallInstruction:
+				platformcall(proc);
 				break;
 			case TerminateInstruction:
 			default:
@@ -179,14 +164,91 @@ void store(processor* proc, uchar dest, uchar src) {
 }
 
 void branch(processor* proc, uvlong dest) {
-	set(proc, ProgramCounter, dest);
+	set(proc, ProgramCounter, dest - 1);
 }
 
 void set(processor* proc, uchar dest, uvlong value) {
-		proc->gpr[dest] = value;
+	proc->gpr[dest] = value;
 }
 void nop(processor* proc) { }
 
 void modop(processor* proc, uchar dest, uchar src0, uchar src1) {
 	proc->gpr[dest] = proc->gpr[src0] % proc->gpr[src1];	
+}
+
+void call(processor* proc, uchar dest) {
+	add(proc, ReturnRegister, ProgramCounter, FalseRegister);
+	branch(proc, proc->gpr[dest]);
+}
+
+void ret(processor* proc) {
+	add(proc, ProgramCounter, ReturnRegister, FalseRegister);
+}
+static void installexitcall(processor* proc);
+static void installputccall(processor* proc);
+static void installgetccall(processor* proc);
+static void installprocessorloop(processor* proc);
+void setupprocessor(processor* proc) {
+
+	/* Upper registers have a special purpose */
+	proc->gpr[FalseRegister] = 0;
+	proc->gpr[TrueRegister] = 1;
+	proc->gpr[ProcessorIdRegister] = processor_count++;
+	proc->gpr[RegisterCountRegister] = 128;
+	proc->gpr[CellCountRegister] = 131072;
+	proc->gpr[ReturnRegister] = 0;
+	/* program-counter */
+	proc->gpr[ProgramCounter] = 2048;
+	installexitcall(proc);
+	installputccall(proc);
+	installgetccall(proc);
+	installprocessorloop(proc);
+}
+void installprocessorloop(processor* proc) {
+	instruction nop;
+	instruction branch0, branch1;
+	/* just loop at this point */
+	nop.value = 0;
+	nop.predicate = TrueRegister;
+	nop.id = NopInstruction;
+	branch0.value = 0;
+	branch0.predicate = TrueRegister;
+	branch0.id = BranchInstruction;
+	branch1.value = proc->gpr[ProgramCounter];
+	proc->memory[proc->gpr[ProgramCounter]] = nop.value;
+
+	proc->memory[proc->gpr[ProgramCounter] + 1] = branch0.value;
+	proc->memory[proc->gpr[ProgramCounter] + 2] = branch1.value;
+}
+void installplatformcallhandler(processor* proc) {
+	/* code is as follows
+	 *
+}
+void installexitcall(processor* proc) {
+	instruction terminate;
+	terminate.value = 0;
+	terminate.id = TerminateInstruction;
+	terminate.predicate = TrueRegister;
+	proc->memory[0] = terminate.value;
+}
+
+void installputccall(processor* proc) {
+	/* code is as follows @ index 1
+	 *
+	 * platform 
+	 * ret
+	 */
+	instruction setA,setB, platform, ret;
+	setA.value = 0;
+	setB.value = 1;
+	platform.value = 0;
+	ret.value = 0;
+	setA.predicate = TrueRegister;
+	setA.id = SetInstruction;
+	platform.predicate = TrueRegister;
+	platform.id = PlatformCallInstruction;
+	proc->memory[1] = setA.value;
+	proc->memory[2] = setB.value;
+	proc->memory[3] = platform.value;
+	proc->memory[4] = ret.value;
 }
--- interpreter.h
+++ interpreter.h
@@ -1,14 +1,26 @@
-
+enum {
+	RegisterCount = 128,
+	MemorySize = 131072,
+};
 /* default processor type */
 uvlong processor_count = 0;
 typedef struct processor {
-	uvlong gpr[128];
-	uvlong memory[131072];
+	uvlong gpr[RegisterCount];
+	uvlong memory[MemorySize];
 } processor;
 
 typedef union instruction {
 	uvlong value;
-	uchar bytes[8];
+	struct {
+		uchar predicate;
+		uchar id;
+		uchar destination0;
+		uchar destination1;
+		uchar source0;
+		uchar source1;
+		uchar byte6;
+		uchar byte7;
+	};
 } instruction;
 
 /* processor register indicies */
@@ -19,19 +31,25 @@ enum {
 	RegisterCountRegister = 124,
 	CellCountRegister = 123,
 	ProgramCounter = 122,
+	ReturnRegister = 121,
+	/* used to jump into the native cpu through a lookup table */
+	PlatformFunctionCallIndex = 120,
+	PlatformOutputRegister0 = 119,
+	PlatformOutputRegister1 = 118,
+	PlatformScratch0 = 117,
+	PlatformScratch1 = 116,
+	PlatformScratch2 = 115,
+	PlatformScratch3 = 114,
+	PlatformScratch4 = 113,
+	PlatformScratch5 = 112,
+	PlatformScratch6 = 111,
+	PlatformScratch7 = 110,
+	PlatformInputRegister0 = 109,
+	PlatformInputRegister1 = 108,
+	PlatformInputRegister2 = 107,
+	PlatformInputRegister3 = 106,
 };
 
-/* instruction components */
-enum {
-	InstructionPredicate = 0,
-	InstructionId = 1,
-	InstructionDestination0 = 2,
-	InstructionDestination1 = 3,
-	InstructionSource0 = 4,
-	InstructionSource1 = 5,
-	InstructionByte6 = 6,
-	InstructionByte7 = 7,
-};
 
 /* instruction translation table */
 enum {
@@ -54,9 +72,22 @@ enum {
 	BranchInstruction = 16,
 	SetInstruction = 17,
 	ModInstruction = 18,
+	CallInstruction = 19,
+	RetInstruction = 20,
+	PlatformCallInstruction = 254,
 	TerminateInstruction = 255,
 };
 
+/* platform calls */
+enum {
+	platformexit = 0,
+	platformputc = 1,
+	platformgetc = 2,
+};
+
+
+
+/* execution processor set */
 void nop(processor* proc);
 void add(processor* proc, uchar dest, uchar src0, uchar src1);
 void sub(processor* proc, uchar dest, uchar src0, uchar src1);
@@ -79,6 +110,8 @@ void store(processor* proc, uchar dest, uchar src);
 
 void branch(processor* proc, uvlong dest);
 void set(processor* proc, uchar dest, uvlong value);
+void call(processor* proc, uchar dest);
+void ret(processor* proc);
 
 /* helper routines */
 void incrementprogramcounter(processor* proc);
@@ -86,5 +119,6 @@ instruction retrieveinstruction(processor* proc);
 int cycle(processor* proc);
 int instructionexecutable(processor* proc, instruction inst);
 
-/* bios routines */
+/* platform routines */
 void setupprocessor(processor* proc);
+void platformcall(processor* proc);
