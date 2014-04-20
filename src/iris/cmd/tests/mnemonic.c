78ee10ffdb1ed0f6326ac9907834364407ddfed5
Joshua Scoggins <Theoretically.x64@gmail.com>
2014-04-19 20:52:33 -0700

    Updated mnemonic.c
    This test only reads the lower 16-bits of the instruction as that is probably
    where instruction bits will be located with the upper 16-bits being used for
    registers and immediates. Doing only the lower 16-bits also ensures that we
    can actually test all of our combinations in a sane amount of time.

--- src/iris/cmd/tests/mnemonic.c
+++ src/iris/cmd/tests/mnemonic.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include "iris.h"
 
 int main() {
-  ushort insn;
-  for(insn = 0; insn < 65535; insn++) {
-    switch(get_group(insn)) {
+  instruction insn;
+  uint i;
+  for(i = 0; i < 65536; i++) {
+    insn.full = i;
+    switch(get_group(&insn)) {
       case InstructionGroupArithmetic:
-        puts(arithmetic_mnemonic(insn));
+        puts(arithmetic_mnemonic(&insn));
         break;
       case InstructionGroupMove:
-        puts(move_mnemonic(insn));
+        puts(move_mnemonic(&insn));
         break;
       case InstructionGroupJump:
-        puts(jump_mnemonic(insn));
+        puts(jump_mnemonic(&insn));
         break;
       case InstructionGroupCompare:
-        puts(compare_mnemonic(insn));
+        puts(compare_mnemonic(&insn));
+        break;
+      case InstructionGroupMisc:
+        puts(misc_mnemonic(&insn));
         break;
       default:
         puts("Unknown instruction group");
+        break;
     }
   }
   return 0;
