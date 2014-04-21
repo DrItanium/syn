8d6e71c06b222c0dd72d135dce0ea2f2e1fe95f2
Joshua Scoggins <Theoretically.x64@gmail.com>
2014-04-20 19:26:50 -0700

    Finished arch documentation update
    Expanded out the op names for implicit registers as well.

--- src/iris/mnemonic.c
+++ src/iris/mnemonic.c
@@ -92,10 +92,10 @@ const char* compare_mnemonic(instruction* insn) {
 const char* misc_mnemonic(instruction* insn) {
    switch(get_misc_op(insn)) {
       case MiscOpSystemCall:                       return "system";
-      case MiscOpSetImplicitRegisterImmediate:     return "implicit.reg.set";
-      case MiscOpSetImplicitRegisterIndirect:      return "implicit.reg.reg.set";
-      case MiscOpGetImplicitRegisterImmediate:     return "implicit.reg.get";
-      case MiscOpGetImplicitRegisterIndirect:      return "implicit.reg.reg.get";
+      case MiscOpSetImplicitRegisterImmediate:     return "implicit.register.set";
+      case MiscOpSetImplicitRegisterIndirect:      return "implicit.register.indirect.set";
+      case MiscOpGetImplicitRegisterImmediate:     return "implicit.register.get";
+      case MiscOpGetImplicitRegisterIndirect:      return "implicit.register.indirect.get";
       default:                                     return "UNKNOWN_MISC";
    }
 
