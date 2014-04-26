11103d60131ed59003ed3e6873c02f998d7eb9b9
Joshua Scoggins <Theoretically.x64@gmail.com>
2014-04-26 16:48:55 -0700

    Added the decode program

---
+++ src/iris/cmd/decode.c
@@ -0,0 +1,39 @@
+/* decode.c - decodes a stream of 32-bit numbers into an iris command */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include "iris.h"
+static int readinstruction(FILE* input, instruction* container);
+int main() {
+   instruction curr;
+   int current, i;
+   uint input;
+   char buffer[80];
+   byte cell;
+   for(i = 0; i < 80; i++) {
+      buffer[i] = 0;
+   }
+   input = 0;
+   while(readinstruction(stdin, &curr)) {
+      unparse(buffer, &curr);
+   }
+   return 0;
+}
+
+int readinstruction(FILE* input, instruction* container) {
+   int a, b, c, d;
+   a = fgetc(input);
+   b = fgetc(input);
+   c = fgetc(input);
+   d = fgetc(input);
+   if(a == EOF || b == EOF || c == EOF || d == EOF) {
+      return 0;
+   } else {
+      container->full = (((d & 0x000000FF) << 24) + ((c & 0x000000FF) << 16) + 
+            ((b & 0x000000FF) << 8) + (a & 0x000000FF));
+
+      return 1;
+   }
+
+}
--- src/iris/config.mk
+++ src/iris/config.mk
@@ -5,9 +5,10 @@ CFLAGS = -g3 -ansi -std=c99 -Wall -I.
 LDFLAGS =  ${LIBS}
 
 BINARY = iris
-RL_BINARY = lnkiris
+RL_BINARY = irislink
+DECODE_BINARY = irisdecode
 
 # The object file that defines main()
 MAIN = cmd/sim.o
-
 RL_MAIN = cmd/img.o
+DECODE_MAIN = cmd/decode.o
