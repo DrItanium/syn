cc35d6ceb163a888b411fa8a09db4fa70154c1a2
Joshua Scoggins <Theoretically.x64@gmail.com>
2014-05-04 00:56:09 -0700

    Finished initial assembler

--- src/iris/config.mk
+++ src/iris/config.mk
@@ -1,12 +1,15 @@
 LIBS = -lc 
 
 CC = cc 
+LEX = flex
+YACC = bison
 CFLAGS = -g3 -ansi -std=c99 -Wall -I.
 LDFLAGS =  ${LIBS}
 
 BINARY = iris
 RL_BINARY = irislink
 DECODE_BINARY = irisdecode
+ASM_BINARY = irisasm
 
 # The object file that defines main()
 MAIN = cmd/sim.o
