19a22bd548a0ec92b3ed2dfffdead4df92267c97
Joshua Scoggins <Theoretically.x64@gmail.com>
2014-05-18 13:16:13 -0700

    There is only one makefile now!

--- src/iris/config.mk
+++ src/iris/config.mk
@@ -3,19 +3,6 @@ LIBS = -lc
 CC = cc 
 LEX = flex
 YACC = bison
-CFLAGS = -g3 -ansi -std=c99 -Wall -Iinclude
-LDFLAGS = ${LIBS}
-
-BINARY = iris
-RL_BINARY = irislink
-DECODE_BINARY = irisdecode
-ASM_BINARY = irisasm
-DBG_BINARY = irisdbg
-
-# The object file that defines main()
-MAIN = cmd/sim.o
-RL_MAIN = cmd/img.o
-DECODE_MAIN = cmd/decode.o
-DBG_MAIN = cmd/dbg.o
-
+CFLAGS = -g3 -ansi -std=c99 -Wall -Iinclude/
+LDFLAGS = ${LIBS} 
 PREFIX = /usr/local
