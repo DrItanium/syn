6aa3c5d0dc3d677c59da0df45de545ef6b3db085
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-12-11 20:40:00 -0800

    Added more templates to Core.h

--- include/Core.h
+++ include/Core.h
@@ -1,7 +1,19 @@
 #ifndef _IRIS_CORE_H
 #define _IRIS_CORE_H
 #include <iostream>
+#include <typeinfo>
 namespace iris {
+	class Core;
+	template<typename T = Core> 
+	struct Describe {
+		static void describe(std::ostream& out) {
+			out << typeid(T).name();
+		}
+		friend std::ostream& operator<<(std::ostream& out, const Describe<T>& c) {
+			describe(out);
+			return out;
+		}
+	};
 	 // Generic iris core interface
 	 class Core {
 		public:
@@ -10,6 +22,15 @@ namespace iris {
 			virtual void shutdown() = 0;
 			virtual void dump(std::ostream& stream) = 0;
 			virtual void run() = 0;
+			// descriptor stream
+			friend std::ostream& operator<<(std::ostream& out, const Core& c) 
+			{
+				c.output(out);
+				return out;
+			}
+		private:
+			// yes this can be overriden in subclasses >_< but not called :D
+			virtual void output( std::ostream& ) const = 0;
 	 };
 }
 #endif
