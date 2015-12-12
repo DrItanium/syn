d715ff3f3717bb398177b36939200c94fad2deff
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-12-11 18:39:22 -0800

    More of encode.cc is c++ized

--- src/libiris/encode.cc
+++ src/libiris/encode.cc
@@ -5,10 +5,40 @@
 #define encode_bits(instruction, mask, value, shiftcount) ((instruction & ~mask) | (value << shiftcount))
 //#define decode_bits(instruction, mask, shiftcount) ((instruction & mask) >> shiftcount)
 #define extract_byte0(a) ((byte)(a & 0x000000FF))
-template<typename T, int mask, int shiftcount>
+template<typename T, instruction mask, int shiftcount>
 T iris_decode_bits(instruction* inst) {
-   return (T)(((*inst & mask) >> shiftcount);
+   return (T)((*inst & mask) >> shiftcount);
 }
+
+template<int offset>
+struct Shifter {
+   static const int shiftcount = 0;
+};
+#define defshiftcount(index, count) \
+   template<> \
+struct Shifter<index> { \
+   static const int shiftcount = count; \
+}
+defshiftcount(0, 0);
+defshiftcount(1, 8);
+defshiftcount(2, 16);
+defshiftcount(3, 24);
+#undef defshiftcount
+
+template<int offset>
+struct FieldBits {
+   static const instruction mask = 0xFFFFFFFF;
+};
+#define deffieldmask(index, bitmask) \
+   template<> \
+   struct FieldBits<index> { \
+      static const instruction mask = bitmask; \
+   }
+deffieldmask(0, 0x000000FF);
+deffieldmask(1, 0x0000FF00);
+deffieldmask(2, 0x00FF0000);
+deffieldmask(3, 0xFF000000);
+#undef deffieldmask
 
 byte iris_decode_group(instruction* inst) {
    return iris_decode_bits<byte, 0x7, 0>(inst);
@@ -17,7 +47,7 @@ void iris_encode_group(instruction* inst, byte group) {
    *inst = encode_bits(*inst, 0x7, group, 0);
 }
 byte iris_decode_op(instruction* inst) {
-   return (byte)decode_bits(extract_byte0(*inst), 0xF8, 3);
+   return iris_decode_bits<byte, 0xF8, 3>(inst);
 }
 void iris_encode_op(instruction* inst, byte value) {
    *inst = encode_bits(*inst, 0xF8, value, 3);
@@ -25,14 +55,16 @@ void iris_encode_op(instruction* inst, byte value) {
 
 byte iris_decode_register(instruction* inst, byte index) {
    switch(index) {
+      case 0:
+         return iris_decode_bits<byte, FieldBits<0>::mask, Shifter<0>::shiftcount>(inst);
       case 1:
-         return (byte)decode_bits(*inst, 0x0000FF00, 8);
+         return iris_decode_bits<byte, FieldBits<1>::mask, Shifter<1>::shiftcount>(inst);
       case 2:
-         return (byte)decode_bits(*inst, 0x00FF0000, 16);
+         return iris_decode_bits<byte, FieldBits<2>::mask, Shifter<2>::shiftcount>(inst);
       case 3:
-         return (byte)decode_bits(*inst, 0xFF000000, 24);
+         return iris_decode_bits<byte, FieldBits<3>::mask, Shifter<3>::shiftcount>(inst);
       default:
-         return (byte)(*inst);
+         iris_error((char*)"illegal field index", 1);
    }
 }
 void iris_encode_register(instruction* inst, byte index, byte value) {
