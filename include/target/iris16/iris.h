5c5790f4645ff72e973542778b99ca4550b314bb
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-12-11 21:19:03 -0800

    Added the DecodedInstruction.cc body

--- include/target/iris16/iris.h
+++ include/target/iris16/iris.h
@@ -1,7 +1,7 @@
 #ifndef _TARGET_IRIS16_IRIS_H
 #define _TARGET_IRIS16_IRIS_H
-#include <iris_base.h>
-#include <Core.h>
+#include "iris_base.h"
+#include "Core.h"
 #include <cstdint>
 namespace iris16 {
 	typedef uint16_t word;
@@ -12,16 +12,30 @@ namespace iris16 {
 		RegisterCount = 256,
 		AddressMax = 65535,
 	};
+	class DecodedInstruction {
+		public:
+			DecodedInstruction();
+			void decode(raw_instruction input);
+#define X(title, mask, shift, type, is_register, post) type get ## title () const { return _ ## post ; }
+#include "target/iris16/instruction.def"
+#undef X
+		private:
+#define X(title, mask, shift, type, is_register, post) type _ ## post ;
+#include "target/iris16/instruction.def"
+#undef X
+	};
 	class Core : public iris::Core {
 		public:
 			Core();
-			~Core();
 			virtual void initialize();
 			virtual void installprogram(std::istream& stream);
 			virtual void shutdown();
 			virtual void dump(std::ostream& stream);
 			virtual void run();
 		private:
+
+		private:
+			DecodedInstruction current;
 			bool execute = true;
 			word gpr[RegisterCount] = {0};
 			word data[AddressMax] = { 0 };
---
+++ src/target/iris16/DecodedInstruction.cc
@@ -0,0 +1,12 @@
+#include "target/iris16/iris.h"
+
+namespace iris16 {
+	DecodedInstruction::DecodedInstruction() { }
+
+	void DecodedInstruction::decode(raw_instruction input) {
+#define X(title, mask, shift, type, is_register, post) \
+		_ ## post = iris::decodeBits<raw_instruction, type, dword, mask, shift>(input);
+#include "target/iris16/instruction.def"
+#undef X
+	}
+}
