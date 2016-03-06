7bd4e90ada09c0f46ff3fb58d39b809f3d217bd9
Joshua Scoggins <Theoretically.x64@gmail.com>
2016-03-06 12:43:53 -0800

    Updated Makefile

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ CC = cc
 CXX = c++
 LEX = flex
 YACC = bison
-GENFLAGS = -Wall -Iinclude/ -g3
+GENFLAGS = -Wall -g3
 CFLAGS = -ansi -std=c99 ${GENFLAGS}
 CXXFLAGS = -std=c++11 ${GENFLAGS}
 LDFLAGS = ${LIBS}
