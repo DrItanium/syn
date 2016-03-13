11eb2fe010298fa244de9e34264b4c1c410dd197
Joshua Scoggins <Theoretically.x64@gmail.com>
2016-03-13 16:03:53 -0700

    Merged Misc into iris16.cc

--- iris16.cc
+++ iris16.cc
@@ -149,4 +149,41 @@ namespace iris16 {
 				break;
 		}
 	}
+	void Core::misc() {
+		switch(static_cast<MiscOp>(current.getOperation())) {
+#define X(name, func) \
+			case MiscOp:: name: \
+			func (); \
+			break;
+#include "misc.def"
+#undef X
+			default:
+				std::cerr << "Illegal misc code " << current.getOperation() << std::endl;
+				execute = false;
+				advanceIp = false;
+				break;
+		}
+	}
+	void Core::systemCall() {
+		switch(static_cast<SystemCalls>(current.getDestination())) {
+			case SystemCalls::Terminate:
+				execute = false;
+				advanceIp = false;
+				break;
+			case SystemCalls::PutC:
+				// read register 0 and register 1
+				std::cout.put((char)gpr[current.getSource0()]);
+				break;
+			case SystemCalls::GetC:
+				byte value;
+				std::cin >> std::noskipws >> value;
+				gpr[current.getSource0()] = (word)value;
+				break;
+			default:
+				std::cerr << "Illegal system call " << current.getDestination() << std::endl;
+				execute = false;
+				advanceIp = false;
+				break;
+		}
+	}
 }
