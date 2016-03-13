65fbf3f2ca5b7e25ea6facbda279d7a52c482597
Joshua Scoggins <Theoretically.x64@gmail.com>
2016-03-13 16:03:25 -0700

    Merged Jump into iris16.cc

--- iris16.cc
+++ iris16.cc
@@ -95,4 +95,58 @@ namespace iris16 {
 				break;
 		}
 	}
+	template<JumpOp op> 
+		struct ConditionalStyle {
+			static const bool isFalseForm = false;
+		};
+#define X(name, ifthenelse, conditional, iffalse, immediate, link) \
+	template<> struct ConditionalStyle<JumpOp:: name> { static const bool isFalseForm = iffalse; };
+#include "jump.def"
+#undef X
+	template<JumpOp op>
+		bool jumpCond(word cond) {
+			return ConditionalStyle<op>::isFalseForm ? (cond == 0) : (cond != 0);
+		}
+
+	void Core::jump() {
+		word newAddr = 0;
+		bool cond = true;
+		advanceIp = false;
+		word ip = gpr[ArchitectureConstants::InstructionPointerIndex];
+		switch(static_cast<JumpOp>(current.getOperation())) {
+#define XImmediateCond_true (current.getImmediate())
+#define XImmediateCond_false (gpr[current.getSource0()])
+#define XIfThenElse_false(immediate) \
+			newAddr = cond ? INDIRECTOR(XImmediateCond, _ ## immediate) : ip + 1;
+#define XIfThenElse_true(immediate) \
+			newAddr = gpr[cond ? current.getSource0() : current.getSource1()];
+#define XImmediateUncond_false (gpr[current.getDestination()])
+#define XImmediateUncond_true (current.getImmediate())
+#define XConditional_false(name, ifthenelse, immediate) \
+			newAddr = INDIRECTOR(XImmediateUncond, _ ## immediate);
+#define XConditional_true(name, ifthenelse, immediate) \
+			cond = jumpCond<JumpOp:: name> (gpr[current.getDestination()]); \
+			INDIRECTOR(XIfThenElse, _ ## ifthenelse)(immediate)
+#define XLink_true \
+			if (cond) { \
+				gpr[ArchitectureConstants::LinkRegisterIndex] = ip + 1; \
+			}
+#define XLink_false
+
+#define X(name, ifthenelse, conditional, iffalse, immediate, link) \
+			case JumpOp:: name: \
+					 { \
+						 INDIRECTOR(XConditional, _ ## conditional)(name, ifthenelse, immediate) \
+						 gpr[ArchitectureConstants::InstructionPointerIndex] = newAddr; \
+						 INDIRECTOR(XLink, _ ## link)  \
+						 break; \
+					 }
+#include "jump.def"
+#undef X
+			default:
+				std::cerr << "Illegal jump code " << current.getOperation() << std::endl;
+				execute = false;
+				break;
+		}
+	}
 }
