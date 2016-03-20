f59c2a8093edfe50e253090632308d837fcd5c33
Joshua Scoggins <Theoretically.x64@gmail.com>
2016-03-20 11:43:05 -0700

    Made copies of all the iris16 files for iris32 purposes

---
+++ iris32.h
@@ -0,0 +1,117 @@
+#ifndef _TARGET_IRIS16_IRIS_H
+#define _TARGET_IRIS16_IRIS_H
+#include "iris_base.h"
+#include "Core.h"
+#include <cstdint>
+typedef uint16_t word;
+typedef uint32_t dword;
+typedef dword raw_instruction;
+typedef word immediate;
+namespace iris16 {
+	enum ArchitectureConstants  {
+		RegisterCount = 256,
+		AddressMax = 65535,
+		InstructionPointerIndex = RegisterCount - 1,
+		LinkRegisterIndex = RegisterCount - 2,
+		StackPointerIndex = RegisterCount - 3,
+		MaxGroups = 0x7,
+		MaxOperations = 0x1F,
+	};
+	dword encodeDword(byte a, byte b, byte c, byte d);
+	word encodeWord(byte a, byte b);
+
+	enum class InstructionGroup : byte {
+#define X(title, _) title,
+#include "iris16_groups.def"
+#undef X
+		Count,
+	};
+	static_assert((byte)InstructionGroup::Count < ((byte)ArchitectureConstants::MaxGroups), "too many instruction groups defined");
+	enum class ArithmeticOp : byte {
+#define X(name, __, ___) name,
+#include "iris16_arithmetic.def"
+#undef X
+		Count
+	};
+	static_assert((byte)ArithmeticOp::Count < ((byte)ArchitectureConstants::MaxOperations), "too many Arithmetic operations defined");
+	enum class MiscOp : byte {
+#define X(title, func) title, 
+#include "iris16_misc.def"
+#undef X
+		Count
+	};
+	static_assert((byte)MiscOp::Count < ((byte)ArchitectureConstants::MaxOperations), "too many Misc operations defined");
+
+	enum class JumpOp : byte {
+#define X(name, ifthenelse, conditional, iffalse, immediate, link) name,
+#include "iris16_jump.def"
+#undef X
+		Count
+	};
+	static_assert((byte)JumpOp::Count < ((byte)ArchitectureConstants::MaxOperations), "too many Jump operations defined");
+
+	enum class SystemCalls : byte {
+#define X(name) name,
+#include "iris16_syscalls.def"
+#undef X
+		Count,
+	};
+	enum class MoveOp : byte {
+#define X(name, type, target, dest, src) name,
+#include "iris16_move.def"
+#undef X
+		Count,
+	};
+	static_assert((byte)MoveOp::Count < ((byte)ArchitectureConstants::MaxOperations), "too many Move operations defined");
+	enum class CompareOp : byte {
+#define X(name, op, group) name,
+#define Y(name, op, group) name,
+#include "iris16_compare.def"
+#undef Y
+#undef X
+		Count,
+	};
+	static_assert((byte)CompareOp::Count < ((byte)ArchitectureConstants::MaxOperations), "too many Compare operations defined");
+	class DecodedInstruction {
+		public:
+			DecodedInstruction();
+			void decode(raw_instruction input);
+#define X(title, mask, shift, type, is_register, post) type get ## title () const { return _ ## post ; }
+#include "iris16_instruction.def"
+#undef X
+		private:
+#define X(title, mask, shift, type, is_register, post) type _ ## post ;
+#include "iris16_instruction.def"
+#undef X
+	};
+	class Core : public iris::Core {
+		public:
+			Core();
+			virtual void initialize();
+			virtual void installprogram(std::istream& stream);
+			virtual void shutdown();
+			virtual void dump(std::ostream& stream);
+			virtual void run();
+			void setInstructionMemory(word address, dword value);
+			void setDataMemory(word address, word value);
+		private:
+			void dispatch();
+#define X(_, op) void op();
+#include "iris16_groups.def"
+#undef X
+#define X(title, func) void func ();
+#include "iris16_misc.def"
+#undef X
+
+		private:
+			DecodedInstruction current;
+			bool execute = true,
+				 advanceIp = true;
+			word gpr[ArchitectureConstants::RegisterCount] = {0};
+			word data[ArchitectureConstants::AddressMax] = { 0 };
+			dword instruction[ArchitectureConstants::AddressMax] = { 0 };
+			word stack[ArchitectureConstants::AddressMax] = { 0 };
+	};
+
+}
+#endif
