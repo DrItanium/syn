249b402524a8d283008f5577c24f39acf2af6afd
Josh Scoggins <jscoggins@parasoft.com>
2016-07-15 16:42:02 -0700

    Allow shell to override the compilers

--- config.mk
+++ config.mk
@@ -1,9 +1,9 @@
 LIBS = -lc -lm
 
-CC = cc 
-CXX = c++
-LEX = flex
-YACC = bison
+CC ?= cc
+CXX ?= c++
+LEX ?= flex
+YACC ?= bison
 GENFLAGS = -Wall -g3
 CFLAGS = -ansi -std=c99 ${GENFLAGS}
 CXXFLAGS = -std=c++11 ${GENFLAGS}
