e604fe96cd4c1f61574f57dc893f77f486b1a776
Joshua Scoggins <Theoretically.x64@gmail.com>
2016-08-26 23:32:50 -0700

    Added the initial phoenix header

---
+++ phoenix.h
@@ -0,0 +1,9 @@
+#ifndef _TARGET_PHOENIX
+#define _TARGET_PHOENIX
+#include <cstdint>
+namespace phoenix {
+	using word = uint16_t;
+	using dword = uint32_t;
+
+} // end namespace phoenix
+#endif // end _TARGET_PHOENIX
