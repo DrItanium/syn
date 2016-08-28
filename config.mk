c3f83c697a50f1bba6922342ca3ed9c92c920062
Joshua Scoggins <Theoretically.x64@gmail.com>
2016-08-28 15:54:59 -0700

    reverted config.mk

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ CC := cc
 CXX := c++
 LEX ?= flex
 YACC ?= bison
-GENFLAGS = -O2 -Wall -g3
+GENFLAGS = -Wall -g3
 CFLAGS = -ansi -std=c99 ${GENFLAGS}
 CXXFLAGS = -std=c++14 ${GENFLAGS}
 LDFLAGS = ${LIBS}
