82f4b6776c02451b06c8afd49fd0e9a5340e0b59
Joshua Scoggins <Theoretically.x64@gmail.com>
2016-08-28 21:39:37 -0700

    Started implementing the phoenix system components

---
+++ Storage.cc
@@ -0,0 +1,48 @@
+#include "phoenix.h"
+
+namespace phoenix {
+	Storage::Storage(Address size) : 
+		_size(size), 
+		_backingStore(new word[size]),
+		_controller(_backingStore, size) 
+	{ 
+		
+	}
+	Storage::~Storage() { }
+	void 
+	Storage::initialize() {
+
+	}
+
+	void
+	Storage::installprogram(std::istream& stream) {
+		// install the controller firmware first
+		_controller.installprogram(stream);
+		// now we need to populate the storage "image"
+		char buf[sizeof(word)] = { 0 };
+		for (auto i = static_cast<Address>(0); i < _size; ++i) {
+			stream.read(buf, sizeof(word));
+			_backingStore.get()[i] = iris::encodeUint16LE(static_cast<byte>(buf[0]), static_cast<byte>(buf[1]));
+		}
+	}
+
+	void 
+	Storage::shutdown() {
+
+	}
+
+	void
+	Storage::dump(std::ostream& stream) {
+		_controller.dump(stream);
+		char buf[sizeof(word)] = { 0 };
+		for (auto i = static_cast<Address>(0); i < _size; ++i) {
+			iris::decodeUint16LE(_backingStore.get()[i], (byte*)buf);
+			stream.write(buf, sizeof(word));
+		}
+	}
+
+	void
+	Storage::run() {
+		_controller.run();
+	}
+}
--- phoenix.h
+++ phoenix.h
@@ -9,22 +9,19 @@ namespace phoenix {
 	using word = uint16_t;
 	using Address = uint32_t;
 
-	// memory controller chip which merges many different memory spaces
-	// together
-	class Core : public iris::Core {
-		using MemorySpace = std::shared_ptr<word>;
+	class Storage : public iris::Core {
 		public:	
-			Core();
-			virtual ~Core();
+			Storage(Address size);
+			virtual ~Storage();
 			virtual void initialize() override;
 			virtual void installprogram(std::istream& stream) override;
 			virtual void shutdown() override;
 			virtual void dump(std::ostream& stream) override;
 			virtual void run() override;
-			virtual void link(std::istream& stream) override;
-			void mapMemorySpace(Address start, Address end, MemorySpace space);
 		private:
-			iris17::Core core;
+			Address _size;
+			std::shared_ptr<word> _backingStore;
+			iris16::Core _controller;
 	};
 
 } // end namespace phoenix
