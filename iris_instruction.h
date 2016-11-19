4d6789cf1ef1220d14825bc02db6207bf39ebcea
Joshua Scoggins <Theoretically.x64@gmail.com>
2016-11-19 12:58:55 -0800

    Simplify the construction of instructions

---
+++ iris_instruction.h
@@ -0,0 +1,29 @@
+#ifndef __IRIS_INSTRUCTION_H
+#define __IRIS_INSTRUCTION_H
+#include "iris_base.h"
+namespace iris {
+	template<typename Word, typename Operation>
+	class Instruction {
+		public:
+			Instruction(Word value) : _rawValue(value) { }
+			~Instruction() { }
+			Word getRawValue() const noexcept { return _rawValue; }
+			Operation getControl() const noexcept { return getControl_impl(); }
+		protected:
+			virtual Operation getControl_impl() const noexcept = 0;
+		protected:
+			Word _rawValue;
+	};
+	template<typename Word, typename Operation, typename RegisterIndex = byte>
+	class RegisterInstruction : public Instruction<Word, Operation> {
+		public:
+			RegisterInstruction(Word value) : Instruction<Word, Operation>(value) { }
+			~RegisterInstruction() { }
+			RegisterIndex getRegister(int index) const { return getRegisterIndex(index); }
+		protected:
+			virtual RegisterIndex getRegisterIndex(int index) const = 0;
+	};
+
+}
+
+#endif // end __IRIS_INSTRUCTION_H
