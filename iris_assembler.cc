5bb1b05fdbc13e1e40fd8ab10bd01a6469108b8b
Josh Scoggins <jscoggins@parasoft.com>
2017-01-12 17:30:22 -0800

    More of iris_assembler implemented

--- iris_assembler.cc
+++ iris_assembler.cc
@@ -4,9 +4,11 @@
 #include <pegtl/analyze.hh>
 #include <pegtl/file_parser.hh>
 #include <pegtl/contrib/raw_string.hh>
+#include <pegtl/contrib/abnf.hh>
 
 namespace iris {
-    struct Comment : pegtl::until<pegtl::eolf > { };
+#define DefSymbol(title, str) \
+    struct Symbol ## title : public pegtl_string_t( #str ) { }
     struct SymbolAdd : public pegtl_string_t( "add" ) { };
     struct SymbolSub : public pegtl_string_t( "sub" ) { };
     struct SymbolMul : public pegtl_string_t( "mul" ) { };
@@ -25,10 +27,10 @@ namespace iris {
     struct SymbolRemImmediate : public pegtl_string_t( "remi" ) { };
     struct SymbolShiftLeftImmediate : public pegtl_string_t("shli") { };
     struct SymbolShiftRightImmediate : public pegtl_string_t("shri") { };
-    struct SymbolArithmeticKeyword : public pegtl::sor<SymbolAdd, SymbolSub, SymbolMul, SymbolDiv, SymbolRem, SymbolShiftLeft, SymbolShiftRight, SymbolAnd, SymbolOr, SymbolNot, SymbolXor, SymbolAddImmediate, SymbolSubImmediate, SymbolMulImmediate, SymbolDivImmediate, SymbolRemImmediate, SymbolShiftLeftImmediate, SymbolShiftRightImmediate> { };
+    DefSymbol(Min, min);
+    DefSymbol(Max, max);
+    struct ArithmeticOperation : public pegtl::sor<SymbolAdd, SymbolSub, SymbolMul, SymbolDiv, SymbolRem, SymbolShiftLeft, SymbolShiftRight, SymbolAnd, SymbolOr, SymbolNot, SymbolXor, SymbolAddImmediate, SymbolSubImmediate, SymbolMulImmediate, SymbolDivImmediate, SymbolRemImmediate, SymbolShiftLeftImmediate, SymbolShiftRightImmediate, SymbolMin, SymbolMax> { };
 
-#define DefSymbol(title, str) \
-    struct Symbol ## title : public pegtl_string_t( #str ) { }
     struct SymbolMove : public pegtl_string_t("move") { };
     struct SymbolSwap : public pegtl_string_t("swap") { };
     struct SymbolSet : public pegtl_string_t("set") { };
@@ -52,7 +54,7 @@ namespace iris {
     DefSymbol(Push, push);
     DefSymbol(PushImmediate, pushi);
     DefSymbol(Pop, pop);
-    struct SymbolManipulationKeyword : public pegtl::sor<SymbolMove, SymbolSwap, SymbolSet, SymbolLoad, SymbolLoadImmediate, SymbolLoadMemory, SymbolLoadWithOffset, SymbolLoadCode, SymbolStore, SymbolStoreImmediate, SymbolStoreWithOffset, SymbolStoreCode, SymbolLoadIO, SymbolStoreIO, SymbolLoadIOWithOffset, SymbolStoreIOWithOffset, SymbolMoveToIP, SymbolMoveFromIP, SymbolMoveToLR, SymbolMoveFromLR, SymbolPush, SymbolPushImmediate, SymbolPop> { };
+    struct ManipulationOperation : public pegtl::sor<SymbolMove, SymbolSwap, SymbolSet, SymbolLoad, SymbolLoadImmediate, SymbolLoadMemory, SymbolLoadWithOffset, SymbolLoadCode, SymbolStore, SymbolStoreImmediate, SymbolStoreWithOffset, SymbolStoreCode, SymbolLoadIO, SymbolStoreIO, SymbolLoadIOWithOffset, SymbolStoreIOWithOffset, SymbolMoveToIP, SymbolMoveFromIP, SymbolMoveToLR, SymbolMoveFromLR, SymbolPush, SymbolPushImmediate, SymbolPop> { };
 
     // branch
     DefSymbol(BranchUnconditionalImmediate, bi);
@@ -77,6 +79,9 @@ namespace iris {
     DefSymbol(BranchLRTrueLink, blrtl);
     DefSymbol(BranchLRFalse, blrf);
     DefSymbol(BranchLRFalseLink, blrfl);
+    struct BranchOperation : public pegtl::sor<SymbolBranchUnconditionalImmediate, SymbolBranchUnconditionalImmediateLink, SymbolBranchUnconditional, SymbolBranchUnconditionalLink, SymbolBranchConditionalTrueImmediate, SymbolBranchConditionalTrueImmediateLink, SymbolBranchConditionalTrue, SymbolBranchConditionalTrueLink, SymbolBranchConditionalFalseImmediate, SymbolBranchConditionalFalseImmediateLink, SymbolBranchConditionalFalse, SymbolBranchConditionalFalseLink, SymbolIfThenElseTrue, SymbolIfThenElseTrueLink, SymbolIfThenElseFalse, SymbolIfThenElseFalseLink, SymbolBranchLR, SymbolBranchLRLink, SymbolBranchLRTrue, SymbolBranchLRTrueLink, SymbolBranchLRFalse, SymbolBranchLRFalseLink> { };
+
+
     // compare operations
     DefSymbol(Eq, eq);
     DefSymbol(Neq, ne);
@@ -90,6 +95,8 @@ namespace iris {
     DefSymbol(GtImmediate, gti);
     DefSymbol(LeImmediate, lei);
     DefSymbol(GeImmediate, gei);
+    struct CompareRegisterOperation : public pegtl::sor< SymbolEq, SymbolNeq, SymbolLt, SymbolGt, SymbolLe, SymbolGe> { };
+    struct CompareImmediateOperation : public pegtl::sor<SymbolEqImmediate, SymbolNeqImmediate, SymbolLtImmediate, SymbolGtImmediate, SymbolLeImmediate, SymbolGeImmediate> { };
 
     // conditional register actions
     DefSymbol(SaveCRs, svcr);
@@ -102,6 +109,7 @@ namespace iris {
     DefSymbol(CRNor, crnor);
     DefSymbol(CRSwap, crswap);
     DefSymbol(CRMove, crmove);
+    struct ConditionalRegisterActionOperation : public pegtl::sor<SymbolSaveCRs, SymbolRestoreCRs, SymbolCRXor, SymbolCRNot, SymbolCRAnd, SymbolCROr, SymbolCRNand, SymbolCRNor, SymbolCRSwap, SymbolCRMove> { };
     // directives
     DefSymbol(LabelDirective, @label);
     DefSymbol(DataDirective, @data);
@@ -110,6 +118,40 @@ namespace iris {
     DefSymbol(DeclareDirective, @declare);
     DefSymbol(HiDirective, @hi);
     DefSymbol(LoDirective, @lo);
-    struct SymbolDirective : public pegtl::sor<SymbolLabelDirective, SymbolDataDirective, SymbolCodeDirective, SymbolOrgDirective, SymbolDeclareDirective, SymbolHiDirective, SymbolLoDirective> { };
+    struct SymbolDirectiveOperation : public pegtl::sor<SymbolLabelDirective, SymbolDataDirective, SymbolCodeDirective, SymbolOrgDirective, SymbolDeclareDirective, SymbolHiDirective, SymbolLoDirective> { };
+
+    // registers
+    // basic define
+
 #undef DefSymbol
+    struct Operation : public pegtl::sor<ConditionalRegisterActionOperation, BranchOperation, ManipulationOperation, ArithmeticOperation, CompareOperation> { };
+    struct Comment : public pegtl::until<pegtl::eolf> { };
+    struct SingleLineComment : public pegtl::disable<pegtl::one<';'>, Comment> { };
+    struct GeneralPurposeRegister : public pegtl::if_must<pegtl::one<'r'>, pegtl::plus<pegtl::digit>> { };
+    struct PredicateRegister : public pegtl::if_must<pegtl::one<'p'>, pegtl::plus<pegtl::digit>> { };
+    struct DestinationGPR : public pegtl::seq<GeneralPurposeRegister> { };
+    struct Source0GPR : public pegtl::seq<GeneralPurposeRegister> { };
+    struct Source1GPR : public pegtl::seq<GeneralPurposeRegister> { };
+    struct SourceRegisters : public pegtl::seq<Source0GPR, Source1GPR> { };
+    struct OneGPR : public pegtl::seq<DestinationGPR> { };
+    struct TwoGPR : public pegtl::seq<DestinationGPR, Source0GPR> { };
+    struct ThreeGPR : public pegtl::seq<DestinationGPR, SourceRegisters> { };
+    struct DestinationPredicateRegister : public pegtl::seq<PredicateRegister> { };
+    struct DestinationPredicateInverseRegister : public pegtl::seq<PredicateRegister> { };
+    struct DestinationPredicate : public pegtl::seq<DestinationPredicateRegister, DestinationPredicateInverseRegister> { };
+    struct Source0Predicate : public pegtl::seq<PredicateRegister> { };
+    struct Source1Predicate : public pegtl::seq<PredicateRegister> { };
+    struct CompareRegister : public pegtl::seq<CompareRegisterOperation, DestinationPredicate, SourceRegisters> { };
+    struct CompareInstruction : public pegtl::seq<CompareRegister, CompareImmediate> { };
+
+    struct Register : public pegtl::sor<GeneralPurposeRegister, PredicateRegsiter> { };
+    struct HexadecimalNumber : public pegtl::if_must<pegtl::istring< '0', 'x'>, pegtl::plus<pegtl::xdigit>> { };
+    struct BinaryNumber : public pegtl::if_must<pegtl::istring<  '0', 'b' >, pegtl::plus<pegtl::abnf::BIT>> { };
+    struct DecimalNumber : public pegtl::seq<pegtl::plus<pegtl::digit>> { };
+    struct Number : public pegtl::sor<HexadecimalNumber, DecimalNumber, Number> { };
+    struct Lexeme : public pegtl::identifier { };
+    struct Instruction : public pegtl::sor<ArithmeticInstruction, CompareInstruction, ConditionalRegisterInstruction, BranchOperation, ManipulationOperation> { };
+    //struct Instruction : public pegtl::seq<Operation, InstructionBits> { };
+    //struct Directive { };
+    //struct Statement : public pegtl::sor<Instruction, Directive> { };
 }
