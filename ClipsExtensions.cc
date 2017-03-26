53ab2089e7bc16d833e7b7ce30ed9aef06dcce22
Joshua Scoggins <Theoretically.x64@gmail.com>
2017-03-25 22:25:37 -0700

    Remove the managed memory block types until they are actually needed
    - This cuts down on program size

--- ClipsExtensions.cc
+++ ClipsExtensions.cc
@@ -543,7 +543,11 @@ namespace syn {
 				}
 			}
 #undef argCheck
-			static void registerWithEnvironment(void* env, const char* title) { ExternalAddressWrapper<Word[]>::registerWithEnvironment(env, title, newFunction, callFunction); }
+
+			static void registerWithEnvironment(void* env, const char* title) { 
+				Parent::registerWithEnvironment(env, title, newFunction, callFunction); 
+			}
+
 			static void registerWithEnvironment(void* env) { 
 				static bool init = true;
 				static std::string func;
@@ -583,11 +587,6 @@ namespace syn {
 	DefWrapperSymbolicName(Block< type > , name ); \
 	using alias = ManagedMemoryBlock< type > 
 	DefMemoryBlock("memory-block:uint16", uint16_t, ManagedMemoryBlock_uint16);
-	DefMemoryBlock("memory-block:uint32", uint32_t, ManagedMemoryBlock_uint32);
-	DefMemoryBlock("memory-block:uint8", uint8_t, ManagedMemoryBlock_uint8);
-	DefMemoryBlock("memory-block:int16", int16_t, ManagedMemoryBlock_int16);
-	DefMemoryBlock("memory-block:int32", int32_t, ManagedMemoryBlock_int32);
-	DefMemoryBlock("memory-block:int8", int8_t, ManagedMemoryBlock_int8);
 #undef DefMemoryBlock
 
 	void installExtensions(void* theEnv) {
@@ -608,12 +607,7 @@ namespace syn {
 		EnvAddUDF(env, "right-shift", "l", CLIPS_shiftRight, "CLIPS_shiftRight", 2, 2, "l;l", nullptr);
 		EnvAddUDF(env, "break-apart-number", "m", CLIPS_breakApartNumber, "CLIPS_breakApartNumber", 1, 1, "l", nullptr);
 		StandardManagedMemoryBlock::registerWithEnvironment(theEnv);
-		ManagedMemoryBlock_uint8::registerWithEnvironment(theEnv);
 		ManagedMemoryBlock_uint16::registerWithEnvironment(theEnv);
-		ManagedMemoryBlock_uint32::registerWithEnvironment(theEnv);
-		ManagedMemoryBlock_int8::registerWithEnvironment(theEnv);
-		ManagedMemoryBlock_int16::registerWithEnvironment(theEnv);
-		ManagedMemoryBlock_int32::registerWithEnvironment(theEnv);
 	}
 
     MultifieldBuilder::MultifieldBuilder(void* env, long capacity) : _size(capacity), _rawMultifield(EnvCreateMultifield(env, capacity)) { }
