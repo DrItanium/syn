cb99f059a8b9717a0e19010e5817c746497fab3e
Josh Scoggins <jscoggins@parasoft.com>
2017-03-27 14:49:12 -0700

    Fixed the compilation bugs relating to bootstrapping the system

--- deffield.sh
+++ deffield.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./syn_repl -f2 lib/cortex.clp -f2 lib/lisp.clp -f2 cmd/deffield.clp $@
+./xsyn_repl -f2 lib/cortex.clp -f2 lib/lisp.clp -f2 cmd/deffield.clp $@
--- deffunction.sh
+++ deffunction.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./syn_repl -f2 lib/cortex.clp -f2 lib/lisp.clp -f2 cmd/deffunctions.clp $@
+./xsyn_repl -f2 lib/cortex.clp -f2 lib/lisp.clp -f2 cmd/deffunctions.clp $@
