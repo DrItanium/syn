63acdde75a2bf8aee22096ec0a43cd220eacfbb3
Josh Scoggins <jscoggins@parasoft.com>
2017-04-14 16:29:42 -0700

    Added specialized cases of div and rem to Base.h

--- Base.h
+++ Base.h
@@ -407,6 +407,167 @@ inline R div(T numerator, T denominator) {
 	}
 }
 
+template<>
+inline uint8_t div<uint8_t, uint8_t>(uint8_t numerator, uint8_t denominator) {
+     switch(denominator) {
+         case 0:
+             throw syn::Problem("Denominator is zero!");
+         case 1: return numerator;
+         case 2: return numerator >> 1;
+         case 4: return numerator >> 2;
+         case 8: return numerator >> 3;
+         case 16: return numerator >> 4;
+         case 32: return numerator >> 5;
+         case 64: return numerator >> 6;
+         case 128: return numerator >> 7;
+         default:
+             return numerator / denominator;
+     }
+}
+
+template<>
+inline uint16_t div<uint16_t, uint16_t>(uint16_t numerator, uint16_t denominator) {
+     switch(denominator) {
+         case 0:
+             throw syn::Problem("Denominator is zero!");
+         case 1: return numerator;
+         case 2: return numerator >> 1;
+         case 4: return numerator >> 2;
+         case 8: return numerator >> 3;
+         case 16: return numerator >> 4;
+         case 32: return numerator >> 5;
+         case 64: return numerator >> 6;
+         case 128: return numerator >> 7;
+         case 256: return numerator >> 8;
+         case 512: return numerator >> 9;
+         case 1024: return numerator >> 10;
+         case 2048: return numerator >> 11;
+         case 4096: return numerator >> 12;
+         case 8192: return numerator >> 13;
+         case 16384: return numerator >> 14;
+         case 32768: return numerator >> 15;
+         default:
+             return numerator / denominator;
+     }
+}
+
+template<>
+inline uint32_t div<uint32_t, uint32_t>(uint32_t numerator, uint32_t denominator) {
+     switch(denominator) {
+         case 0:
+             throw syn::Problem("Denominator is zero!");
+         case 1: return numerator;
+         case 2: return numerator >> 1;
+         case 4: return numerator >> 2;
+         case 8: return numerator >> 3;
+         case 16: return numerator >> 4;
+         case 32: return numerator >> 5;
+         case 64: return numerator >> 6;
+         case 128: return numerator >> 7;
+         case 256: return numerator >> 8;
+         case 512: return numerator >> 9;
+         case 1024: return numerator >> 10;
+         case 2048: return numerator >> 11;
+         case 4096: return numerator >> 12;
+         case 8192: return numerator >> 13;
+         case 16384: return numerator >> 14;
+         case 32768: return numerator >> 15;
+         case 65536: return numerator >> 16;
+         case 131072: return numerator >> 17;
+         case 262144: return numerator >> 18;
+         case 524288: return numerator >> 19;
+         case 1048576: return numerator >> 20;
+         case 2097152: return numerator >> 21;
+         case 4194304: return numerator >> 22;
+         case 8388608: return numerator >> 23;
+         case 16777216: return numerator >> 24;
+         case 33554432: return numerator >> 25;
+         case 67108864: return numerator >> 26;
+         case 134217728: return numerator >> 27;
+         case 268435456: return numerator >> 28;
+         case 536870912: return numerator >> 29;
+         case 1073741824: return numerator >> 30;
+         case 2147483648: return numerator >> 31;
+         default:
+             return numerator / denominator;
+     }
+}
+
+template<>
+inline uint64_t div<uint64_t, uint64_t>(uint64_t numerator, uint64_t denominator) {
+     switch(denominator) {
+         case 0:
+             throw syn::Problem("Denominator is zero!");
+         case 1: return numerator;
+         case 2: return numerator >> 1;
+         case 4: return numerator >> 2;
+         case 8: return numerator >> 3;
+         case 16: return numerator >> 4;
+         case 32: return numerator >> 5;
+         case 64: return numerator >> 6;
+         case 128: return numerator >> 7;
+         case 256: return numerator >> 8;
+         case 512: return numerator >> 9;
+         case 1024: return numerator >> 10;
+         case 2048: return numerator >> 11;
+         case 4096: return numerator >> 12;
+         case 8192: return numerator >> 13;
+         case 16384: return numerator >> 14;
+         case 32768: return numerator >> 15;
+         case 65536: return numerator >> 16;
+         case 131072: return numerator >> 17;
+         case 262144: return numerator >> 18;
+         case 524288: return numerator >> 19;
+         case 1048576: return numerator >> 20;
+         case 2097152: return numerator >> 21;
+         case 4194304: return numerator >> 22;
+         case 8388608: return numerator >> 23;
+         case 16777216: return numerator >> 24;
+         case 33554432: return numerator >> 25;
+         case 67108864: return numerator >> 26;
+         case 134217728: return numerator >> 27;
+         case 268435456: return numerator >> 28;
+         case 536870912: return numerator >> 29;
+         case 1073741824: return numerator >> 30;
+         case 2147483648: return numerator >> 31;
+         case 4294967296: return numerator >> 32;
+         case 8589934592: return numerator >> 33;
+         case 17179869184: return numerator >> 34;
+         case 34359738368: return numerator >> 35;
+         case 68719476736: return numerator >> 36;
+         case 137438953472: return numerator >> 37;
+         case 274877906944: return numerator >> 38;
+         case 549755813888: return numerator >> 39;
+         case 1099511627776: return numerator >> 40;
+         case 2199023255552: return numerator >> 41;
+         case 4398046511104: return numerator >> 42;
+         case 8796093022208: return numerator >> 43;
+         case 17592186044416: return numerator >> 44;
+         case 35184372088832: return numerator >> 45;
+         case 70368744177664: return numerator >> 46;
+         case 140737488355328: return numerator >> 47;
+         case 281474976710656: return numerator >> 48;
+         case 562949953421312: return numerator >> 49;
+         case 1125899906842624: return numerator >> 50;
+         case 2251799813685248: return numerator >> 51;
+         case 4503599627370496: return numerator >> 52;
+         case 9007199254740992: return numerator >> 53;
+         case 18014398509481984: return numerator >> 54;
+         case 36028797018963968: return numerator >> 55;
+         case 72057594037927936: return numerator >> 56;
+         case 144115188075855872: return numerator >> 57;
+         case 288230376151711744: return numerator >> 58;
+         case 576460752303423488: return numerator >> 59;
+         case 1152921504606846976: return numerator >> 60;
+         case 2305843009213693952: return numerator >> 61;
+         case 4611686018427387904: return numerator >> 62;
+         case 9223372036854775807: return numerator >> 63;
+         default:
+             return numerator / denominator;
+     }
+}
+
+
 template<typename T, typename R = T>
 inline R rem(T numerator, T denominator) {
 	if (denominator == 0) {
@@ -416,6 +577,167 @@ inline R rem(T numerator, T denominator) {
 	} else {
 		return static_cast<R>(numerator % denominator);
 	}
+}
+template<>
+inline uint8_t rem<uint8_t, uint8_t>(uint8_t numerator, uint8_t denominator) {
+    switch(denominator) {
+        case 0:
+            throw syn::Problem("Denominator is zero!");
+        case 1: return 0;
+        case 2: return numerator & 1;
+        case 4: return numerator & 3;
+        case 8: return numerator & 7;
+        case 16: return numerator & 15;
+        case 32: return numerator & 31;
+        case 64: return numerator & 63;
+        case 128: return numerator & 127;
+        default:
+            return numerator % denominator;
+    }
+}
+
+template<>
+inline uint16_t rem<uint16_t, uint16_t>(uint16_t numerator, uint16_t denominator) {
+    switch(denominator) {
+        case 0:
+            throw syn::Problem("Denominator is zero!");
+        case 1:
+            return 0;
+        case 2: return numerator & 1;
+        case 4: return numerator & 3;
+        case 8: return numerator & 7;
+        case 16: return numerator & 15;
+        case 32: return numerator & 31;
+        case 64: return numerator & 63;
+        case 128: return numerator & 127;
+        case 256: return numerator & 255;
+        case 512: return numerator & 511;
+        case 1024: return numerator & 1023;
+        case 2048: return numerator & 2047;
+        case 4096: return numerator & 4095;
+        case 8192: return numerator & 8191;
+        case 16384: return numerator & 16383;
+        case 32768: return numerator & 32767;
+        default:
+            return numerator % denominator;
+    }
+}
+
+template<>
+inline uint32_t rem<uint32_t, uint32_t>(uint32_t numerator, uint32_t denominator) {
+    switch(denominator) {
+        case 0:
+            throw syn::Problem("Denominator is zero!");
+        case 1:
+            return 0;
+        case 2: return numerator & 1;
+        case 4: return numerator & 3;
+        case 8: return numerator & 7;
+        case 16: return numerator & 15;
+        case 32: return numerator & 31;
+        case 64: return numerator & 63;
+        case 128: return numerator & 127;
+        case 256: return numerator & 255;
+        case 512: return numerator & 511;
+        case 1024: return numerator & 1023;
+        case 2048: return numerator & 2047;
+        case 4096: return numerator & 4095;
+        case 8192: return numerator & 8191;
+        case 16384: return numerator & 16383;
+        case 32768: return numerator & 32767;
+        case 65536: return numerator & 65535;
+        case 131072: return numerator & 131071;
+        case 262144: return numerator & 262143;
+        case 524288: return numerator & 524287;
+        case 1048576: return numerator & 1048575;
+        case 2097152: return numerator & 2097151;
+        case 4194304: return numerator & 4194303;
+        case 8388608: return numerator & 8388607;
+        case 16777216: return numerator & 16777215;
+        case 33554432: return numerator & 33554431;
+        case 67108864: return numerator & 67108863;
+        case 134217728: return numerator & 134217727;
+        case 268435456: return numerator & 268435455;
+        case 536870912: return numerator & 536870911;
+        case 1073741824: return numerator & 1073741823;
+        case 2147483648: return numerator & 2147483647;
+        default:
+            return numerator % denominator;
+    }
+}
+template<>
+inline uint64_t rem<uint64_t, uint64_t>(uint64_t numerator, uint64_t denominator) {
+    switch(denominator) {
+        case 0:
+            throw syn::Problem("Denominator is zero!");
+        case 1:
+            return 0;
+        case 2: return numerator & 1;
+        case 4: return numerator & 3;
+        case 8: return numerator & 7;
+        case 16: return numerator & 15;
+        case 32: return numerator & 31;
+        case 64: return numerator & 63;
+        case 128: return numerator & 127;
+        case 256: return numerator & 255;
+        case 512: return numerator & 511;
+        case 1024: return numerator & 1023;
+        case 2048: return numerator & 2047;
+        case 4096: return numerator & 4095;
+        case 8192: return numerator & 8191;
+        case 16384: return numerator & 16383;
+        case 32768: return numerator & 32767;
+        case 65536: return numerator & 65535;
+        case 131072: return numerator & 131071;
+        case 262144: return numerator & 262143;
+        case 524288: return numerator & 524287;
+        case 1048576: return numerator & 1048575;
+        case 2097152: return numerator & 2097151;
+        case 4194304: return numerator & 4194303;
+        case 8388608: return numerator & 8388607;
+        case 16777216: return numerator & 16777215;
+        case 33554432: return numerator & 33554431;
+        case 67108864: return numerator & 67108863;
+        case 134217728: return numerator & 134217727;
+        case 268435456: return numerator & 268435455;
+        case 536870912: return numerator & 536870911;
+        case 1073741824: return numerator & 1073741823;
+        case 2147483648: return numerator & 2147483647;
+        case 4294967296: return numerator & 4294967295;
+        case 8589934592: return numerator & 8589934591;
+        case 17179869184: return numerator & 17179869183;
+        case 34359738368: return numerator & 34359738367;
+        case 68719476736: return numerator & 68719476735;
+        case 137438953472: return numerator & 137438953471;
+        case 274877906944: return numerator & 274877906943;
+        case 549755813888: return numerator & 549755813887;
+        case 1099511627776: return numerator & 1099511627775;
+        case 2199023255552: return numerator & 2199023255551;
+        case 4398046511104: return numerator & 4398046511103;
+        case 8796093022208: return numerator & 8796093022207;
+        case 17592186044416: return numerator & 17592186044415;
+        case 35184372088832: return numerator & 35184372088831;
+        case 70368744177664: return numerator & 70368744177663;
+        case 140737488355328: return numerator & 140737488355327;
+        case 281474976710656: return numerator & 281474976710655;
+        case 562949953421312: return numerator & 562949953421311;
+        case 1125899906842624: return numerator & 1125899906842623;
+        case 2251799813685248: return numerator & 2251799813685247;
+        case 4503599627370496: return numerator & 4503599627370495;
+        case 9007199254740992: return numerator & 9007199254740991;
+        case 18014398509481984: return numerator & 18014398509481983;
+        case 36028797018963968: return numerator & 36028797018963967;
+        case 72057594037927936: return numerator & 72057594037927935;
+        case 144115188075855872: return numerator & 144115188075855871;
+        case 288230376151711744: return numerator & 288230376151711743;
+        case 576460752303423488: return numerator & 576460752303423487;
+        case 1152921504606846976: return numerator & 1152921504606846975;
+        case 2305843009213693952: return numerator & 2305843009213693951;
+        case 4611686018427387904: return numerator & 4611686018427387903;
+        case 9223372036854775807: return numerator & 9223372036854775807;
+        default:
+            return numerator % denominator;
+    }
 }
 template<typename T, typename R = bool>
 constexpr R eq(T a, T b) noexcept {
