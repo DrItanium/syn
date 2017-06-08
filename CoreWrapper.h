8eec65b6fb86518e6b871259a42654d01b8f9c93
Joshua Scoggins <Theoretically.x64@gmail.com>
2017-06-08 07:06:58 -0700

    Added a generic core wrapper!

---
+++ CoreWrapper.h
@@ -0,0 +1,78 @@
+/*
+ * syn
+ * Copyright (c) 2013-2017, Joshua Scoggins and Contributors
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+// Common CLIPS <-> C++ core wrapper
+#ifndef CORE_WRAPPER_H__
+#define CORE_WRAPPER_H__
+
+#include "ClipsExtensions.h"
+
+namespace syn {
+
+template<typename T>
+class CoreWrapper : public syn::ExternalAddressWrapper<T> {
+    public:
+        using Parent = syn::ExternalAddressWrapper<T>;
+        using Self = CoreWrapper<T>;
+    public:
+        static bool callFunction(void* env, syn::DataObjectPtr value, syn::DataObjectPtr ret) {
+            static bool init = true;
+            static std::string funcErrorPrefix;
+            if (init) {
+                init = false;
+                auto functions = syn::retrieveFunctionNames<T>;
+                funcErrorPrefix = std::get<2>(functions);
+            }
+            if (!syn::isExternalAddress(value)) {
+                return syn::errorMessage(env, "CALL", 1, funcErrorPrefix, "Function call expected an external address as the first argument!");
+            }
+            auto ptr = static_cast<Self*>(EnvDOPToExternalAddress(value));
+            return ptr->get()->handleOperation(env, ret);
+        }
+        static void registerWithEnvironment(void* env, const char* title) {
+            Parent::registerWithEnvironment(env, title, callFunction);
+        }
+
+        static void registerWithEnvironment(void* env) {
+            static bool init = true;
+            static std::string func;
+            if (init) {
+                init = false;
+                func = Self::getType();
+            }
+            registerWithEnvironment(env, func.c_str());
+        }
+   public:
+        CoreWrapper(T* core) : Parent(core) { }
+        CoreWrapper() : Parent(new T()) { }
+        virtual ~CoreWrapper() { }
+
+};
+
+} // end namespace syn
+
+
+#endif // end CORE_WRAPPER_H__
