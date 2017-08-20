f9caa1b0e1696a1f7c88af53dc094b23304b84b5
Joshua Scoggins <Theoretically.x64@gmail.com>
2017-08-20 13:19:12 -0700

    Added the header file for the common assembler wrapper type

---
+++ CommonAssemblerWrapper.h
@@ -0,0 +1,39 @@
+/**
+ * @file
+ * Common assembler wrapper interface type, provides common functionality for
+ * CLIPS <-> native interaction when parsing assembly
+ * @copyright
+ * syn
+ * Copyright (c) 2013-2017, Joshua Scoggins and Contributors
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#ifndef __SYN_COMMON_ASSEMBLER_WRAPPER_H
+#define __SYN_COMMON_ASSEMBLER_WRAPPER_H
+#include "ClipsExtensions.h"
+
+namespace syn {
+
+} // end namespace syn
+
+#endif // end __SYN_COMMON_ASSEMBLER_WRAPPER_H
