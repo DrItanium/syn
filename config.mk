21404a5bb0b7f43955b56721e02ec30f9090c76f
Joshua Scoggins <Theoretically.x64@gmail.com>
2017-08-20 15:03:02 -0700

    fixed the config.mk to actually use proper changes

--- config.mk
+++ config.mk
@@ -1,9 +1,9 @@
 LIBS = -lc -lm -lboost_system -lboost_filesystem -lpthread
 
-CC ?= cc
-CXX ?= c++
-LEX ?= flex
-YACC ?= bison
+CC := cc
+CXX := c++
+LEX := flex
+YACC := bison
 GENFLAGS = -Wall -g3 -Imisc/maya/ -Imisc/PEGTL/include
 CFLAGS = -ansi -std=c99 ${GENFLAGS}
 CXXFLAGS = -std=c++14 ${GENFLAGS}
