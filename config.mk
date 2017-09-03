e2201f4de0d99388afe42eb674c05365ba6f6e9d
Joshua Scoggins <Theoretically.x64@gmail.com>
2017-09-02 18:04:11 -0700

    No need to set LEX and YACC

--- config.mk
+++ config.mk
@@ -2,8 +2,6 @@ LIBS = -lc -lm -lboost_system -lboost_filesystem -lpthread
 
 CC := cc
 CXX := c++
-LEX := flex
-YACC := bison
 GENFLAGS = -Wall -g3 -Imisc/maya/ -Imisc/PEGTL/include
 CFLAGS = -ansi -std=c99 ${GENFLAGS}
 CXXFLAGS = -std=c++14 ${GENFLAGS}
