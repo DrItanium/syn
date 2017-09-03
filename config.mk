ff874948dfa39e3b4cebb154aa2e9178c3b56fac
Joshua Scoggins <Theoretically.x64@gmail.com>
2017-09-02 18:29:37 -0700

    disable optimizations

--- config.mk
+++ config.mk
@@ -2,7 +2,7 @@ LIBS = -lc -lm -lboost_system -lboost_filesystem -lpthread
 
 CC := cc
 CXX := c++
-GENFLAGS = -Wall -g3 -Imisc/maya/ -Imisc/PEGTL/include
+GENFLAGS = -Wall -g3 -Imisc/maya/ -Imisc/PEGTL/include 
 CFLAGS = -ansi -std=c99 ${GENFLAGS}
 CXXFLAGS = -std=c++14 ${GENFLAGS}
 LDFLAGS = ${LIBS}
