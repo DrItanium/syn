c3848932432a6cda51b97cacd258a22a2a3c814d
Joshua Scoggins <Theoretically.x64@gmail.com>
2017-10-02 15:30:22 -0700

    updated syn.sh to not use the autocompletion code

--- syn.sh
+++ syn.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-rlwrap -pPurple --remember -c -f misc/completions/maya.rlwrap -f misc/completions/syn.rlwrap -m -M .clp ./syn $@
+#rlwrap -pPurple --remember -c -f misc/completions/maya.rlwrap -f misc/completions/syn.rlwrap -m -M .clp ./syn $@
+# the completions don't work right since CLIPS can use hyphens :(
+rlwrap -pPurple -c -m -M .clp ./syn $@
