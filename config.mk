3cc4cfd39ed40df37a4317c4a29d91256de08335
Joshua Scoggins <Theoretically.x64@gmail.com>
2017-10-06 18:46:18 -0700

    fix config.mk

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 LIBS = -lc -lm -lboost_system -lboost_filesystem -lpthread -lrt
 
-CC := clang
-CXX := clang++
+CC := cc
+CXX := c++
 GENFLAGS = -Wall -g3 -Imisc/maya/ -Imisc/PEGTL/include
 CFLAGS = -ansi -std=c99 ${GENFLAGS}
 CXXFLAGS = -std=c++14 ${GENFLAGS}
