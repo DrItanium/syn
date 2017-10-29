12ac497406fc205e73e88bd769c1300717e0b323
Joshua Scoggins <Theoretically.x64@gmail.com>
2017-10-29 14:02:14 -0700

    Implemented more functions for sound card support

--- AlsaMIDIExtensions.cc
+++ AlsaMIDIExtensions.cc
@@ -28,7 +28,7 @@
 
 #include "AlsaMIDIExtensions.h"
 extern "C" {
-	#include "clips.h"
+#include "clips.h"
 }
 #include <alsa/asoundlib.h>
 
@@ -36,72 +36,129 @@ extern "C" {
 #include "ClipsExtensions.h"
 
 namespace syn {
-void listSoundCards(UDFContext* context, CLIPSValue* ret);
-void installAlsaMIDIExtensions(void* theEnv) {
-	Environment* env = (Environment*)theEnv;
-	EnvAddUDF(env, "list-sound-cards", "v", listSoundCards, "listSoundCards", 0, 0, nullptr, nullptr);
-}
-using SoundCardId = int;
-using SoundCardStatusCode = int;
-void listSoundCards(UDFContext* context, CLIPSValue* ret) {
-	// an adaption of the code found at https://ccrma.stanford.edu/~craig/articles/linuxmidi/alsa-1.0/alsarawportlist.c
-	SoundCardId card = -1;
-	SoundCardStatusCode status;
-	status = snd_card_next(&card);
-	if (status < 0) {
+	void listSoundCards(UDFContext* context, CLIPSValue* ret);
+	void listMidiPorts(UDFContext* context, CLIPSValue* ret);
+	void installAlsaMIDIExtensions(void* theEnv) {
+		Environment* env = (Environment*)theEnv;
+		EnvAddUDF(env, "list-sound-cards", "v", listSoundCards, "listSoundCards", 0, 0, nullptr, nullptr);
+		EnvAddUDF(env, "list-midi-ports", "v", listMidiPorts, "listMidiPorts", 0, 0, nullptr, nullptr);
+	}
+	using SoundCardId = int;
+	using SoundCardStatusCode = int;
+	void soundCardError(UDFContext* context, CLIPSValue* ret, int code, const std::string& desc, SoundCardStatusCode status) {
 		CVSetBoolean(ret, false);
 		std::string cardId (snd_strerror(status));
 		std::stringstream msg;
-		msg << "cannot determine card number: " << cardId;
+		msg << desc << ": " << cardId;
 		auto str2 = msg.str();
-		errorMessage(context, "SYSTEM", 1, "sound card error: ", str2);
-		return;
+		errorMessage(context, "SYSTEM", code, "sound card error: ", str2);
 	}
-	if (card < 0) {
-		errorMessage(context, "SYSTEM", 1, "sound card error: ", "no sound cards found");
+
+	void soundCardError(UDFContext* context, CLIPSValue* ret, int code, const std::string& desc) {
 		CVSetBoolean(ret, false);
-		return;
+		errorMessage(context, "SYSTEM", code, "sound card error: ", desc);
 	}
-	auto theEnv = UDFContextEnvironment(context);
-	while (card >= 0) {
-		std::stringstream msg;
-		msg << "Card " << card << ":" << std::endl;
-		auto cardId = msg.str();
-		EnvPrintRouter(theEnv, WDISPLAY, cardId.c_str());
-		msg.str("");
-		char* longName = nullptr;
-		char* shortName = nullptr;
-		status = snd_card_get_name(card, &shortName);
-		if (status < 0) {
-			msg << "cannot determine card shortname: " << snd_strerror(status) << std::endl;
-			auto badMsg = msg.str();
-			EnvPrintRouter(theEnv, WDISPLAY, badMsg.c_str());
-			CVSetBoolean(ret, false);
-			return;
-		}
-		status = snd_card_get_longname(card, &longName);
-		if (status < 0) {
-			msg << "cannot determine card longname: " << snd_strerror(status) << std::endl;
-			auto badMsg = msg.str();
-			EnvPrintRouter(theEnv, WDISPLAY, badMsg.c_str());
-			CVSetBoolean(ret, false);
-			return;
-		}
-		msg << "\tLong name: " << longName << std::endl;
-		msg << "\tShort name: " << shortName << std::endl;
-		auto names = msg.str();
-		EnvPrintRouter(theEnv, WDISPLAY, names.c_str());
+	void noSoundCardsFoundError(UDFContext* context, CLIPSValue* ret) {
+		soundCardError(context, ret, 1, "no sound cards found!");
+	}
+	void unableToDetermineCardNumberError(UDFContext* context, CLIPSValue* ret, SoundCardStatusCode status) {
+		soundCardError(context, ret, 1, "cannot determine card number", status);
+	}
+	void listSoundCards(UDFContext* context, CLIPSValue* ret) {
+		// an adaption of the code found at https://ccrma.stanford.edu/~craig/articles/linuxmidi/alsa-1.0/alsarawportlist.c
+		SoundCardId card = -1;
+		SoundCardStatusCode status;
 		status = snd_card_next(&card);
 		if (status < 0) {
-			msg.str("");
-			msg << "cannot determine card number: " << snd_strerror(status) << std::endl;
-			auto tmp = msg.str();
-			EnvPrintRouter(theEnv, WDISPLAY, tmp.c_str());
-			CVSetBoolean(ret, false);
+			unableToDetermineCardNumberError(context, ret, status);
 			return;
 		}
+		if (card < 0) {
+			noSoundCardsFoundError(context, ret);
+			return;
+		}
+		auto theEnv = UDFContextEnvironment(context);
+		while (card >= 0) {
+			std::stringstream msg;
+			msg << "Card " << card << ":" << std::endl;
+			auto cardId = msg.str();
+			EnvPrintRouter(theEnv, WDISPLAY, cardId.c_str());
+			msg.str("");
+			char* longName = nullptr;
+			char* shortName = nullptr;
+			status = snd_card_get_name(card, &shortName);
+			if (status < 0) {
+				soundCardError(context, ret, 2, "cannot determine card shortname", status);
+				return;
+			}
+			status = snd_card_get_longname(card, &longName);
+			if (status < 0) {
+				soundCardError(context, ret, 2, "cannot determine card longname", status);
+				return;
+			}
+			msg << "\tLong name: " << longName << std::endl;
+			msg << "\tShort name: " << shortName << std::endl;
+			auto names = msg.str();
+			EnvPrintRouter(theEnv, WDISPLAY, names.c_str());
+			status = snd_card_next(&card);
+			if (status < 0) {
+				unableToDetermineCardNumberError(context, ret, status);
+				return;
+			}
+		}
+		CVSetBoolean(ret, true);
 	}
-	CVSetBoolean(ret, true);
-}
+	using SoundController = snd_ctl_t;
+	using RawMidiDeviceId = int;
+	void listMidiDevicesOnCard(UDFContext* context, CLIPSValue* ret, SoundCardId card, const char* logicalName) {
+		SoundController* ctl;
+		RawMidiDeviceId device = -1;
+		SoundCardStatusCode status;
+		std::stringstream msg;
+		msg << "hw:" << card;
+		auto name = msg.str();
+		status = snd_ctl_open(&ctl, name.c_str(), 0);
+		if (status < 0) {
+			msg.str("");
+			msg << "cannot open control for card " << card;
+			auto e = msg.str();
+			soundCardError(context, ret, 1, e, status);
+			return;
+		}
+		do {
+			status = snd_ctl_rawmidi_next_device(ctl, &device);
+			if (status < 0) {
+				soundCardError(context, ret, 2, "cannot determine device number", status);
+				return;
+			}
+			// TODO: continue this
+		} while (device >= 0);
+	}
+	void listMidiPorts(UDFContext* context, CLIPSValue* ret) {
+		SoundCardStatusCode status;
+		SoundCardId card = -1;  // use -1 to prime the pump of iterating through card list
+
+		status = snd_card_next(&card);
+		if (status < 0) {
+			unableToDetermineCardNumberError(context, ret, status);
+			return;
+		}
+		if (card < 0) {
+			noSoundCardsFoundError(context, ret);
+			return;
+		}
+		auto theEnv = UDFContextEnvironment(context);
+		EnvPrintRouter(theEnv, WDISPLAY, "\nType Device    Name\n");
+		EnvPrintRouter(theEnv, WDISPLAY, "====================================\n");
+		while (card >= 0) {
+			list_midi_devices_on_card(card);
+			if ((status = snd_card_next(&card)) < 0) {
+				unableToDetermineCardNumberError(context, ret, status);
+				return;
+			}
+		} 
+		EnvPrintRouter
+		printf("\n");
+	}
 
 } // end namespace syn
